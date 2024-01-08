9db8ebb400d0d8564a92d0c234313e7c2a34896a
Cain <134607924+CainC0de@users.noreply.github.com>
2024-01-08 11:07:07 -0300

    Update gerar_senha.py
    um novo design foi adicionado ao codigo, implementando um texto em ASCII para deixar o programa visualmente mais bonito.

--- gerar_senha.py
+++ gerar_senha.py
@@ -1,16 +1,30 @@
+from termcolor import colored
 import random
 import string
-from termcolor import colored
+
+def exibir_logo_passly():
+    print (colored(r"""
+$$$$$$$\                           $$\                     $$\                       $$$$$$\ $$\   $$\$$$$$$\$$\   $$\ 
+$$  __$$\                          $$ |                    $$ |                     $$  __$$\$$ |  $$ \_$$  _$$$\  $$ |
+$$ |  $$ $$$$$$\  $$$$$$$\ $$$$$$$\$$ |    $$\   $$\       $$$$$$$\ $$\   $$\       $$ /  \__$$ |  $$ | $$ | $$$$\ $$ |
+$$$$$$$  \____$$\$$  _____$$  _____$$ |    $$ |  $$ |      $$  __$$\$$ |  $$ |      $$ |     $$$$$$$$ | $$ | $$ $$\$$ |
+$$  ____/$$$$$$$ \$$$$$$\ \$$$$$$\ $$ |    $$ |  $$ |      $$ |  $$ $$ |  $$ |      $$ |     \_____$$ | $$ | $$ \$$$$ |
+$$ |    $$  __$$ |\____$$\ \____$$\$$ |    $$ |  $$ |      $$ |  $$ $$ |  $$ |      $$ |  $$\      $$ | $$ | $$ |\$$$ |
+$$ |    \$$$$$$$ $$$$$$$  $$$$$$$  $$$$$$$$\$$$$$$$ |      $$$$$$$  \$$$$$$$ |      \$$$$$$  |     $$ $$$$$$\$$ | \$$ |
+\__|     \_______\_______/\_______/\________\____$$ |      \_______/ \____$$ |       \______/      \__\______\__|  \__|
+                                           $$\   $$ |               $$\   $$ |                                         
+                                           \$$$$$$  |               \$$$$$$  |                                         
+                                            \______/                 \______/                                          
+    """, 'green'))
 
 def gerar_senha_forte(tamanho=20):
-    # Lista de caracteres que serão usados na senha
     caracteres = string.ascii_letters + string.digits + string.punctuation
-
-    # Gerar senha aleatória
     senha = ''.join(random.choice(caracteres) for _ in range(tamanho))
-
     return senha
+
+# Chame a função para exibir o logo PassLy
+exibir_logo_passly()
 
 # Gerar senha forte com 20 caracteres
 senha_forte = gerar_senha_forte()
-print(colored("Senha forte gerada:", "green"), senha_forte)
+print(colored("Senha gerada:", "green"), senha_forte)
